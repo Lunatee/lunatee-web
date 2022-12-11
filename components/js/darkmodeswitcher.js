d6a8fb10a3549246e6febd60dbe7cf657a3967d6
SunightMC <108746731+SunightMC@users.noreply.github.com>
2022-12-11 13:35:12 +0100

    Delete darkmodeswitcher.js

--- components/js/darkmodeswitcher.js
+++
@@ -1,6 +0,0 @@
-function switchDark() {
-  var intro = document.getElementsByClassName("intro");
-  var content = document.getElementsByClassName("content");
-  intro.classList.toggle("dark-mode-intro");
-  content.classList.toggle("dark-mode-content");
-} 
